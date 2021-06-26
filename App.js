e48d58b97b14f0c7504073da9a17322e759262f3
Neel Patanwadia <patanwadia2497@gmail.com>
2021-06-26 19:30:29 +0530

    Screen Created for netflix

--- App.js
+++ App.js
@@ -7,26 +7,19 @@
  */
 import 'react-native-gesture-handler';
 import React from 'react';
-import {View, Text} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import Netflix from './app/screens/netflix/Netflix';
 
 const Stack = createStackNavigator();
 
-const HomeScreen = () => {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator headerMode={'none'}>
+        <Stack.Screen name="Netflix" component={Netflix} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ app/screens/netflix/Netflix.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SafeAreaView, Text, FlatList, ScrollView} from 'react-native';
+
+const Netflix = () => {
+  return (
+    <SafeAreaView>
+      <Text>Netflix</Text>
+    </SafeAreaView>
+  );
+};
+export default Netflix;
